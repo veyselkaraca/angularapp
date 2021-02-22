0a7887e11920ea67553cea8742ee11b8d31f4450
Veysel <veysel.karaca@gazi.edu.tr>
2021-02-22 14:24:32 +0300

    routing-test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyfirstcomponentComponent } from './myfirstcomponent/myfirstcomponent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"/first",
+    component:MyfirstcomponentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyfirstcomponentComponent } from './myfirstcomponent/myfirstcomponent.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      MyfirstcomponentComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
